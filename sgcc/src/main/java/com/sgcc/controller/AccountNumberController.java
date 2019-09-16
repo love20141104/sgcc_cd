a69513b3142c28a81d1b07c36ed821568549daba
gaoshize <350665690@qq.com>
2019-09-17 06:15:04 +0800

    Merge branch 'start0903' of https://github.com/sgcc20190802/sgcc_cd into start0903
    # Conflicts:
    #	sgcc/src/main/resources/application.yml

---
+++ sgcc/src/main/java/com/sgcc/controller/AccountNumberController.java
@@ -0,0 +1,25 @@
+package com.sgcc.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "", tags = "户号管理")
+@RestController
+@RequestMapping(value = "/AccountNumber")
+@Controller
+public class AccountNumberController {
+
+    /**
+     * 绑定户号
+     */
+
+    /**
+     * 解邦户号
+     */
+
+    /**
+     * 已绑定
+     */
+}
