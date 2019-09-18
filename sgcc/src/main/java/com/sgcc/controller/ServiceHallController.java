6409fef3ffb5e0daccd0351dd296299e0e1487c0
gaoshize <350665690@qq.com>
2019-09-18 16:54:02 +0800

    domain-model

--- sgcc/src/main/java/com/sgcc/controller/ServiceHallController.java
+++ sgcc/src/main/java/com/sgcc/controller/ServiceHallController.java
@@ -19,21 +19,54 @@ import java.util.List;
 
 @Api(value = "", tags = "地图接口")
 @RestController
-@RequestMapping(value = "/map")
+@RequestMapping(value = "/ServiceHall")
 @Controller
-public class MapController {
+public class ServiceHallController {
     @Autowired
     private ServiceHallService serviceHallService;
 
+    /**
+     * 根据定位查最近的5个
+     * @param lat
+     * @param lng
+     * @return
+     */
     @ApiOperation(value = "Nearest ServiceHall", notes = "")
     @GetMapping(value = "/NearestServiceHalls")
     public Result getNearestServiceHalls(@RequestParam Double lat,@RequestParam Double lng) {
         return serviceHallService.NearestServiceHalls(lat,lng);
     }
 
+    /**
+     * 查行政区域所有营业厅
+     * @param district
+     * @return
+     */
     @ApiOperation(value = "ServiceHall District", notes = "")
     @GetMapping(value = "/Districts/{district}")
     public Result getNearestServiceHalls(@PathVariable String district) {
         return serviceHallService.serviceHalls(district);
     }
+
+    /**
+     * 根据用户id查询所有的预约信息
+     * @param openId
+     * @return
+     */
+    @ApiOperation(value = "预约信息", notes = "")
+    @GetMapping(value = "/PrebookInfos/user/{openId}")
+    public Result getPrebookInfos(@PathVariable String openId) {
+        return null;
+    }
+
+    /**
+     * 用户提交在线预约
+     * @return
+     */
+//    @ApiOperation(value = "提交预约信息", notes = "")
+//    @PostMapping(value = "/PrebookInfo")
+//    public Result submitPrebookInfo(@ResponseBody ) {
+//
+//        return null;
+//    }
 }
