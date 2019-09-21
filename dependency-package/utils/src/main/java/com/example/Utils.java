c186787379bb022cb99dd554c7b72a1413ef2c88
riham <253073323@qq.com>
2019-09-21 23:42:32 +0800

    意见提交,未测试

--- dependency-package/utils/src/main/java/com/example/Utils.java
+++ dependency-package/utils/src/main/java/com/example/Utils.java
@@ -9,6 +9,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utils {
     /**
@@ -58,6 +60,24 @@ public class Utils {
             e.printStackTrace();
         }
         return string1;
+    }
+    public static boolean verifyUrl(String url){
+        if( Strings.isNullOrEmpty(url) )
+            return false;
+        //转换为小写
+        url = url.toLowerCase();
+        String regex = "^((https|http|ftp|rtsp|mms)?://)"  //https、http、ftp、rtsp、mms
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+                + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184
+                + "|" // 允许IP和DOMAIN（域名）
+                + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.
+                + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名
+                + "[a-z]{2,6})" // first level domain- .com or .museum
+                + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+                + "((/?)|" // a slash isn't required if there is no file name
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+        return  url.matches(regex);
+
     }
 
     public static String GetCurrentTime()
--- mq/mq/src/main/java/com/sgcc/amqp/consumer/SuggestionComsumer.java
+++ mq/mq/src/main/java/com/sgcc/amqp/consumer/SuggestionComsumer.java
@@ -1,7 +1,10 @@
 package com.sgcc.amqp.consumer;
 
+import com.google.common.base.Strings;
+import com.sgcc.FastDFSClient.FastDFSClient;
 import com.sgcc.dao.SuggestionDao;
 import com.sgcc.dao.SuggestionRedisDaos;
+import com.sgcc.entity.WeChatEntity;
 import com.sgcc.entity.query.SuggestionEventEntity;
 import com.sgcc.model.SuggestionModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SuggestionComsumer {
-
+    @Autowired
+    private WeChatEntity weChatEntity;
     @Autowired
     private SuggestionEventEntity suggestionEventEntity;
 
@@ -18,14 +22,27 @@ public class SuggestionComsumer {
     public void Save( SuggestionDao dao ){
         try{
             // Todo 下载
-
-            // Todo 更新Dao
-
+            dao.setImg_1(uploadFile(dao.getImg_1())); ;
+            dao.setImg_2(uploadFile(dao.getImg_2())); ;
+            dao.setImg_3(uploadFile(dao.getImg_3())); ;
             // Todo 持久化
             suggestionEventEntity.Save(dao);
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+    private String uploadFile( String mediaId )
+    {
+        if( Strings.isNullOrEmpty(mediaId) )
+        {
+            return "";
+        }
+        byte[] ctnt = weChatEntity.downloadMedia(mediaId);
+        if( ctnt == null ||ctnt.length < 1 )
+        {
+            return "";
+        }
+        return new FastDFSClient().uploadFile( new String(ctnt) ,".jpg");
     }
 
     @JmsListener(destination = "Suggestion_mq_s")
