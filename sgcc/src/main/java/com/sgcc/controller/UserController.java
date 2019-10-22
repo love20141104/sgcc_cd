5098b4aa81a979ea4466b710678ae2171004c2f5
xiemin <1419594692@qq.com>
2019-10-22 13:54:13 +0800

    20191022pm

--- sgcc/src/main/java/com/sgcc/controller/UserController.java
+++ sgcc/src/main/java/com/sgcc/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.sgcc.controller;
 
 import com.example.result.Result;
+import com.sgcc.dto.OrderTransDTO;
+import com.sgcc.service.RecordService;
 import com.sgcc.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,21 +13,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 
 
-@Api(value = "", tags = "月度账单接口")
+@Api(value = "", tags = "用户相关业务接口")
 @RestController
-@RequestMapping(value = "/bill")
+@RequestMapping(value = "/user")
 @Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RecordService recordService;
+
     @ApiOperation(value = "查询月度账单", notes = "")
-    @GetMapping(value = "/billInfo/{userNo}")
+    @GetMapping(value = "/bill/billInfo/{userNo}")
     public Result queryBillInfo(@PathVariable(required = true) String userNo,
                                 @RequestParam(required = true) long date) {
         return userService.queryBillInfoById(userNo,date);
     }
+
+
+    @ApiOperation(value = "查询缴费记录", notes = "")
+    @GetMapping(value = "/record/recordInfo/{userNo}")
+    public Result queryRecordInfo(@PathVariable(required = true) String userNo) {
+        return recordService.queryRecordInfo(userNo);
+    }
+
+    @ApiOperation(value = "订单信息", notes = "")
+    @PostMapping(value = "/record/orderInfo")
+    public Result orderInfo(@RequestBody(required = true) OrderTransDTO orderTransDTO) {
+        return recordService.queryOrderInfo(orderTransDTO);
+    }
+
+
 
 
 }
