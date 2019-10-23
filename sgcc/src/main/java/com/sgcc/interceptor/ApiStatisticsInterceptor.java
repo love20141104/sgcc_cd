3bcc5654410f5714b3bd9cabcc9cd412b7052029
Rapeal-Dekkers <2499784331@qq.com>
2019-10-23 20:18:19 +0800

    10.23 添加接口统计模块

---
+++ sgcc/src/main/java/com/sgcc/interceptor/ApiStatisticsInterceptor.java
@@ -0,0 +1,89 @@
+package com.sgcc.interceptor;
+
+import com.sgcc.dao.ApiStatisticsDao;
+import com.sgcc.producer.ApiStatisticsProducer;
+import org.elasticsearch.common.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.UUID;
+
+public class ApiStatisticsInterceptor implements HandlerInterceptor {
+    @Autowired
+    private ApiStatisticsProducer apiStatisticsProducer;
+    static Logger logger = LoggerFactory.getLogger(ApiStatisticsInterceptor.class);
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        String userOpenId=request.getParameter("userOpenId");
+        if(!Strings.isNullOrEmpty(userOpenId)){
+            // 记录下请求内容
+            String apiUrl=request.getRequestURL().toString();
+            String clientIp=getIpAddress();
+            ApiStatisticsDao apiStatisticsDao = new ApiStatisticsDao(UUID.randomUUID().toString(), apiUrl, userOpenId, new Date(), clientIp);
+            //发mq
+            apiStatisticsProducer.apiStatisticsMQ(apiStatisticsDao);
+            logger.info(apiStatisticsDao.toString());
+        }
+
+    }
+
+    public static HttpServletRequest getHttpServletRequest() {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return servletRequestAttributes.getRequest();
+    }
+    //获取ip
+    public static String getIpAddress() {
+        HttpServletRequest request = getHttpServletRequest();
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+        }else if (ip != null && ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (int index = 0; index < ips.length; index++) {
+                String strIp = (String) ips[index];
+                if (!("unknown".equalsIgnoreCase(strIp))) {
+                    ip = strIp;
+                    break;
+                }
+            }
+        }
+        return ip;
+    }
+
+
+}
