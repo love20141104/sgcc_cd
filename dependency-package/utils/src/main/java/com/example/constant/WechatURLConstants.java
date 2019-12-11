ca3b97515943caa6aa48b9ffe6ee5c746d9adc46
253073323@qq.com <Riham0502>
2019-12-12 01:03:58 +0800

    1.改回索思科得

--- dependency-package/utils/src/main/java/com/example/constant/WechatURLConstants.java
+++ dependency-package/utils/src/main/java/com/example/constant/WechatURLConstants.java
@@ -9,15 +9,15 @@ public class WechatURLConstants {
 
     //public static final String APPID = "wxe5aa7f870094050d";    //电享+
 
-    //public static final String APPID = "wxe6c880a3994d6443";  // 索思科得
+    public static final String APPID = "wxe6c880a3994d6443";  // 索思科得
 
-    public static final String APPID = "wx02b80a55b779f33b";  // 成都供电
+//    public static final String APPID = "wx02b80a55b779f33b";  // 成都供电
 
     //private static final String SECRET = "949ad61412c16d82b58dba6c49cee038";    //电享+
 
-//    private static final String SECRET = "c0629cd108bf6f8d60d4aa31f4f6484a";  // 索思科得
+    private static final String SECRET = "c0629cd108bf6f8d60d4aa31f4f6484a";  // 索思科得
 
-    private static final String SECRET = "a0a8a45cfcb735662fadeaef46c40c14";  // 成都供电
+//    private static final String SECRET = "a0a8a45cfcb735662fadeaef46c40c14";  // 成都供电
 
     public static final  String  GETACCESSTOKEN = BASE+"/cgi-bin/token?grant_type=client_credential&appid="+APPID+"&secret="+SECRET;
 
@@ -41,6 +41,7 @@ public class WechatURLConstants {
 
     //public static final  String mch_id = "1550585661";      // 电享+
     public static final  String mch_id = "1555016091";    // 索思科得
+
     public static final  String APPKEY = "SSCDGDabcdefghijklmnopqrstuvwxyz";
 
     public static final String HMACSHA256 = "HMAC-SHA256";
